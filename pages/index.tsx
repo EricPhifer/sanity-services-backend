e851ff1520fdee378a6ec52dd54635346390dff2
Eric Phifer <ericoncode@gmail.com>
2022-04-12 14:22:56 -0600

    change index.tsx to match index.ts - trying to skip country select page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,46 @@
-import Layout from '@components/Layout'
-import { cmsList } from '@utils/cms'
+import sanityClient from '@sanity/client'
 import _ from 'lodash'
-import { searchEngineList } from '@utils/search'
-import { cmsFunctions } from '@utils/cms'
-import { GetStaticProps } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-type Props = {
-  [key: string]: any
-  countries: any[]
-  cms: 'sanity'
-  searchEngine?: 'algolia'
-}
-
-const IndexPage = (props: Props) => {
-  const { cms } = props
-  return (
-    <Layout title="Eric Phifer LLC - Services" showMenu={false} cms={cms}>
-      
-    </Layout>
- 
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
+  const client = sanityClient({
+    projectId: process.env.SANITY_PROJECT_ID as string,
+    dataset: process.env.SANITY_DATASET as string,
+    token: process.env.SANITY_TOKEN as string, // or leave blank to be anonymous user
+    useCdn: false, // `false` if you want to ensure fresh data
+  })
+  const query = `*[_type == "variant"]`
+  const items = await client.fetch<any[]>(query)
+  const update = await Promise.all(
+    items.map((item) => {
+      return client
+        .patch(item._id)
+        .set({
+          size: _.first(item.size),
+          //   name: {
+          //     en_us: item.name,
+          //     it_it: item.name,
+          //   },
+          //   // label: {
+          //   //   en_us: item.label,
+          //   //   it_it: item.label,
+          //   // },
+          //   // slug: {
+          //   //   _type: 'localeSlug',
+          //   //   en_us: {
+          //   //     _type: 'slug',
+          //   //     current: item.name.toLowerCase().replaceAll(' ', '-'),
+          //   //   },
+          //   //   it_it: {
+          //   //     _type: 'slug',
+          //   //     current: item.name.toLowerCase().replaceAll(' ', '-'),
+          //   //   },
+          //   // },
+        })
+        .commit()
+    })
   )
+  return res.json({
+    items: update,
+  })
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const cms = cmsList()
-  const searchEngine = searchEngineList()
-  const countries = _.has(cmsFunctions, `${cms}AllCountries`)
-    ? await cmsFunctions[`${cms}AllCountries`]()
-    : []
-  return {
-    props: {
-      countries,
-      cms,
-      searchEngine,
-    },
-    revalidate: false,
-  }
-}
-
-export default IndexPage
